b1db6e84c19f9f8d3bad8eebaa8b26ee9ee17800
Mkranj <mkranjcev6@gmail.com>
2023-11-25 16:27:00 +0100

    Condense rows of multiple console releases of same game

--- 1_load.R
+++ 1_load.R
@@ -9,6 +9,21 @@ original <- read.csv("Video_Games_Sales_as_at_22_Dec_2016.csv")
 # Release year as numeric. Missing coded as N/A - okay to be lost.
 original$Year_release_n <- as.numeric(original$Year_of_Release)
 
+# The rows are per release on console. So a multiple game could have
+# multiple rows. We need to condense that.
+games <- group_by(original, Name) %>%
+  summarise(
+    Platform = paste(Platform, collapse = "; "),
+    # If there are multiple release years for multiple editions, count the earliest.
+    Year_of_Release = min(Year_of_Release),
+    Genre = Genre[1],
+    Publisher = Publisher[1],
+    # For copies sold, sum across editions
+    NA_Sales = sum(NA_Sales, na.rm = T),
+    Global_Sales = sum(Global_Sales, na.rm = T),
+    Developer = Developer[1],
+    Rating = Rating[1]
+  )
 
 # Factors
 rating_main_categories <- c("E", "E10+", "M", "T")
