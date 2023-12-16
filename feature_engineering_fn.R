91ca49f5c4e5c5e43ec4b4c834cfdd2d221e62ad
Mkranj <mkranjcev6@gmail.com>
2023-12-16 11:03:02 +0100

    Unknown devs - set avg sales and no games to zero

--- feature_engineering_fn.R
+++ feature_engineering_fn.R
@@ -16,3 +16,17 @@ count_subtitles <- function(game_names){
     subtitle_count = subtitle_count
   ))
 }
+
+#' Turn missing values to zeroes
+#'
+#' @param dataset DF to process
+#' @param column_name Character, name of column to process
+#'
+#' @return Dataset with adjusted column
+missing_to_zero <- function(dataset, column_name) {
+  data <- dataset
+  values <-  data[[column_name]]
+  values[is.na(values)] <- 0
+  data[[column_name]] <- values
+  data
+}
